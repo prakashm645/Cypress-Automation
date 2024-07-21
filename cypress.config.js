451e1258f54b5dc905372606d9a78ccfbf7e209a
Prakash Madhesh <prakash.m@bolt.earth>
2024-07-21 12:58:14 +0530

    My cypress framwork test

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  defaultCommandTimeout: 8000,
+  env:
+   {
+    url : "https://rahulshettyacademy.com"
+   },
+  projectId: '6mtabm',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    video : true,
+    videoFolder : 'cypress/reports/html/video',
+    screenshotsFolder : 'cypress/reports/html/screenshots',
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+    specPattern : 'cypress/integration/examples/*.js'
+  },
+});
